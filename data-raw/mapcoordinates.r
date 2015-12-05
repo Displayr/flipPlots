0ea22f3cb2e30603cafc7bedc8c7265257b62d7a
TimBock <tim.bock@numbers.net.au>
2015-12-05 12:18:54 +1100

    zzz

--- data-raw/mapcoordinates.r
+++ data-raw/mapcoordinates.r
@@ -4,8 +4,7 @@ library(rgdal)
 # Code source: http://stackoverflow.com/questions/29118059/display-spatialpolygonsdataframe-on-leaflet-map-with-r
 download.file(file.path('http://www.naturalearthdata.com/http/',
                         'www.naturalearthdata.com/download/50m/cultural',
-                        'ne_50m_admin_0_countries.zip'),
-f <- tempfile())
+                        'ne_50m_admin_0_countries.zip'), f <- tempfile())
 unzip(f, exdir=tempdir())
 library(rgdal)
 map.coordinates <- readOGR(tempdir(), 'ne_50m_admin_0_countries', encoding='UTF-8')
