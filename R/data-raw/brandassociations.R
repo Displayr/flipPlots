e041aff8a02ee3b3a422c12d5499b6b90194570f
TimBock <tim.bock@numbers.net.au>
2015-12-22 17:43:20 +1100

    Brand associations data

--- R/data-raw/brandassociations.R
+++ R/data-raw/brandassociations.R
@@ -1,11 +1,9 @@
 #brand.associations = system.file("extdata", "brandassoc.csv", package="flipPlots")
-
-
-
 brand.associations <- read.csv("C:/Users/Tim/Documents/GitHub/flipPlots/inst/extdata/brandassoc.csv",
                               row.names = 1)
-brand.associations <- matrix(as.numeric(as.matrix(z)), nrow = nrow(z),
-                             dimnames = dimnames(z))
+devtools::use_data(csd.perceptions, internal = FALSE, overwrite = TRUE)
+#brand.associations <- matrix(as.numeric(as.matrix(z)), nrow = nrow(z),
+#                             dimnames = dimnames(z))
 
 
 brand.correlations <- cor(t(brand.associations))
