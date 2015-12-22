4edb23ee7f2e55c2606d53d7d42ec7c015e48be8
Tim Keighley <Tim.Keighley@numbers.net.au>
2015-12-22 17:10:33 +1100

    Enable tests

--- tests/testthat/testworldmap.R
+++ tests/testthat/testworldmap.R
@@ -4,33 +4,31 @@ context("World Map")
 
 
 
-# test_that("Checking inputs",
-# {
-#     invalid.continent.names <- 1:6
-#     names(invalid.continent.names) <- c("Asia", "Africa", "Europe", "South America", "Oceania", "North America")
-#     expect_error(WorldMap(invalid.continent.names))
-#     rm(invalid.continent.names)
-#     expect_error(WorldMap(1:10))
-#     expect_error(WorldMap(matrix(1, 2, 2)))
-#     expect_error(WorldMap(matrix(1, 2, 2, dimnames = list(LETTERS[1:2], NULL))))
-#     expect_error(WorldMap(matrix(1, 2, 2, dimnames = list(NULL, LETTERS[1:2]))))
-#     expect_error(WorldMap(array(1, dim = c(2,2,2),
-#         dimnames = list(LETTERS[1:2], LETTERS[1:2], LETTERS[1:2]))))
-# })
-#
-#
-# test_that("Checking accuracy of row names",
-# {
-#     valid.continent.names <- 1:6
-#     names(valid.continent.names) <- c("Asia", "Africa", "Europe", "South America", "Oceania", "North America")
-#     expect_that(WorldMap(valid.continent.names, type = "continent"), not(throws_error()))
-#     rm(valid.continent.names)
-#     invalid.continent.names <- 1:6
-#     names(invalid.continent.names) <- c("Asia", "Africag", "Europe", "South America", "Oceania", "North America")
-#     expect_error(WorldMap(invalid.continent.names, type = "continent"))
-#     invalid.country.names <- matrix(1:2, 2,dimnames =list(c("Australia", "New Zealands"), "A"))
-#     expect_error(WorldMap(invalid.country.names))
-#     rm(invalid.country.names)
-# })
+test_that("Checking inputs",
+{
+    invalid.continent.names <- 1:6
+    names(invalid.continent.names) <- c("Asia", "Africa", "Europe", "South America", "Oceania", "North America")
+    expect_error(WorldMap(invalid.continent.names))
+    rm(invalid.continent.names)
+    expect_error(WorldMap(1:10))
+    expect_error(WorldMap(matrix(1, 2, 2)))
+    expect_error(WorldMap(matrix(1, 2, 2, dimnames = list(LETTERS[1:2], NULL))))
+    expect_error(WorldMap(matrix(1, 2, 2, dimnames = list(NULL, LETTERS[1:2]))))
+    expect_error(WorldMap(array(1, dim = c(2,2,2),
+        dimnames = list(LETTERS[1:2], LETTERS[1:2], LETTERS[1:2]))))
+})
 
 
+test_that("Checking accuracy of row names",
+{
+    valid.continent.names <- 1:6
+    names(valid.continent.names) <- c("Asia", "Africa", "Europe", "South America", "Oceania", "North America")
+    expect_that(WorldMap(valid.continent.names, type = "continent"), not(throws_error()))
+    rm(valid.continent.names)
+    invalid.continent.names <- 1:6
+    names(invalid.continent.names) <- c("Asia", "Africag", "Europe", "South America", "Oceania", "North America")
+    expect_error(WorldMap(invalid.continent.names, type = "continent"))
+    invalid.country.names <- matrix(1:2, 2,dimnames =list(c("Australia", "New Zealands"), "A"))
+    expect_error(WorldMap(invalid.country.names))
+    rm(invalid.country.names)
+})
