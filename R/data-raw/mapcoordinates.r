ff355b429acff302a888e65e80df558bca2e6602
Tim Keighley <Tim.Keighley@numbers.net.au>
2015-12-23 16:34:25 +1100

    CE-73: Maps using choroplethr.

--- R/data-raw/mapcoordinates.r
+++ R/data-raw/mapcoordinates.r
@@ -9,3 +9,13 @@ unzip(f, exdir=tempdir())
 library(rgdal)
 map.coordinates <- readOGR(tempdir(), 'ne_50m_admin_0_countries', encoding='UTF-8')
 devtools::use_data(map.coordinates, internal = FALSE, overwrite = TRUE)
+
+
+f <- tempfile()
+download.file(file.path('http://www.naturalearthdata.com/http/',
+    'www.naturalearthdata.com/download/10m/cultural',
+    'ne_10m_admin_1_states_provinces.zip'), f)
+d <- tempdir()
+unzip(f, exdir = d)
+admin1.coordinates <- rgdal::readOGR(d, 'ne_10m_admin_1_states_provinces', encoding = 'UTF-8')
+devtools::use_data(admin1.coordinates, internal = FALSE, overwrite = TRUE)
