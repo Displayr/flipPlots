17cf555fad7353a9ced6e89bfdbc96b57299e2a3
Tim Keighley <Tim.Keighley@numbers.net.au>
2016-01-08 17:13:34 +1100

    Move data documentation to its own file.

--- R/data.R
+++ R/data.R
@@ -1,10 +1,13 @@
-
+#' Admin-0 level coordinates for the world
+#'
 #' @format A SpatialPolygonsDataFrame with data frame with 177 rows and 63 columns:
 #' @source \url{http://www.naturalearthdata.com/downloads/50m-cultural-vectors/}
+#' @rdname data
 "map.coordinates"
 
-#' World coordinates at the Admin-1 level
+#' Admin-1 level coordinates for the world
 #'
 #' @format A SpatialPolygonsDataFrame with data frame with 4647 rows and 59 columns:
 #' @source \url{http://www.naturalearthdata.com/downloads/10m-cultural-vectors/}
+#' @rdname data
 "admin1.coordinates"
