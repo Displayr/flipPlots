72cde7f83829dec238dd2433655b2e2b51496c46
TimBock <tim.bock@numbers.net.au>
2016-05-10 15:40:58 +1000

    Committing file by file to get past github bug

--- R/data-raw/brandassociations.R
+++ R/data-raw/brandassociations.R
@@ -4,3 +4,14 @@ brand.associations <- read.csv("C:/Users/Tim/Documents/GitHub/flipPlots/inst/ext
 devtools::use_data(brand.associations, internal = FALSE, overwrite = TRUE)
 #brand.associations <- matrix(as.numeric(as.matrix(z)), nrow = nrow(z),
 #                             dimnames = dimnames(z))
+
+
+data("brand.associations")
+brand.correlations <- cor(t(brand.associations))
+brand.sim <- 1 - brand.correlations
+zcoords <- as.data.frame(MASS::isoMDS(brand.sim)$points)
+colnames(zcoords) <- c("A","N")
+
+## Labeled Scatterplot regression tests
+# default
+LabeledScatterPlot(zcoords)
