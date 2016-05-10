12f42520c709545202603c67ea4d230cfa708b12
TimBock <tim.bock@numbers.net.au>
2016-05-10 15:38:16 +1000

    Committing file by file to get past github bug

--- R/data.R
+++ R/data.R
@@ -1,63 +1,48 @@
-#' Admin-0 level coordinates for the world at 1:50m resolution
-#'
-#' @format A SpatialPolygonsDataFrame with data frame with 241 rows and 63 columns.
-#' @source \url{http://www.naturalearthdata.com/downloads/50m-cultural-vectors/}
-#' @rdname data
-"map.coordinates.50"
-
-#' Admin-0 level coordinates for the world at 1:110m resolution
-#'
-#' @format A SpatialPolygonsDataFrame with data frame with 177 rows and 63 columns.
-#' @source \url{http://www.naturalearthdata.com/downloads/110m-cultural-vectors/}
-#' @rdname data
-"map.coordinates.110"
-
-#' Obsolete Admin-0 level coordinates for the world.
-#'
-#' Use \code{map.coordinates} rather than this.
-#'
-#' @format A SpatialPolygonsDataFrame with data frame with 177 rows and 63 columns.
-#' @source \url{https://raw.githubusercontent.com/datasets/geo-boundaries-world-110m/master/countries.geojson}
-#' @rdname data
-"country.coordinates"
+#' #' Admin-0 level coordinates for the world
+#' #'
+#' #' @format A SpatialPolygonsDataFrame with data frame with 177 rows and 63 columns:
+#' #' @source \url{http://www.naturalearthdata.com/downloads/50m-cultural-vectors/}
+#' #' @rdname data
+#' "map.coordinates"
 
 #' Admin-1 level coordinates for the world
 #'
-#' @format A SpatialPolygonsDataFrame with data frame with 4647 rows and 59 columns.
+#' @format A SpatialPolygonsDataFrame with data frame with 4647 rows and 59 columns:
 #' @source \url{http://www.naturalearthdata.com/downloads/10m-cultural-vectors/}
 #' @rdname data
 "admin1.coordinates"
 
-#' GDP of German states for 2008 to 2013
+#' Admin-1 level coordinates for the world
 #'
-#' @format A data frame with 96 rows and 4 columns.
-#' @source \url{https://en.wikipedia.org/wiki/List_of_German_states_by_GDP}
+#' @format A SpatialPolygonsDataFrame with data frame with 4647 rows and 59 columns:
+#' @source \url{http://www.naturalearthdata.com/downloads/10m-cultural-vectors/}
 #' @rdname data
-"german.state.gdp"
+"country.coordinates"
 
-#' Brand associations
+#'  Brand Associations
 #'
-#' @format A data frame with 42 rows and 15 columns.
-#' @source Q example data
+#' @format A \link{\code{matrix}} containing binary associations between brands and attributes.
+#' @source Numbers International Pty Ltd.
 #' @rdname data
 "brand.associations"
 
-#' Cola perceptions
+#'  Brand Associations
 #'
-#' @format A numeric matrix with 7 rows and 9 columns.
-#' @source Q example data
+#' @format A \link{\code{matrix}} containing binary associations between cola brands and attributes.
+#' @source Numbers International Pty Ltd.
 #' @rdname data
 "cola.perceptions"
 
-#' CSD perceptions
+#'  CSD Associations
 #'
-#' @format A numeric matrix with 8 rows and 11 columns.
-#' @source Q example data
+#' @format A \link{\code{matrix}} containing binary associations between CSD brands and attributes.
+#' @source Numbers International Pty Ltd.
 #' @rdname data
 "csd.perceptions"
 
-#' Q default color scheme
+#'  Default Q chart types.
 #'
-#' @format A vector of 14 color strings.
+#' @format A \link{\code{vector}} containing RGB colors.
+#' @source Numbers International Pty Ltd.
 #' @rdname data
 "q.colors"
